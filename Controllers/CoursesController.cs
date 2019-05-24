55e2219f74666ff150bc1668c1a17ec07c68a9a8
Giacomo R <giacomorosati87@gmail.com>
2019-05-24 17:24:51 +0200

    chore: add css style,money,currency,models

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -1,10 +1,19 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using MyCourse.Models.Services.Application;
+using MyCourse.Models.ViewModels;
 
 namespace MyCourse.Controllers
 {
     public class CoursesController : Controller
     {
-        public IActionResult Index() => View();
+        public IActionResult Index()
+        {
+            /*var */CourseService courseService = new CourseService();
+            List<CourseViewModel> courses = courseService.GetServices();
+            return View(courses);
+        }
+
         public IActionResult Detail(string id)
         {
             return View();
